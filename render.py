cfba460f70b3b0d7ed7c24be68813323bf9d2c13
haohuynhhcmvn <haohuynh.hcm.vn@gmail.com>
2026-03-22 17:52:43 +0700

    Update render.py

--- render.py
+++ render.py
@@ -4,9 +4,15 @@
 # ==============================
 
 from moviepy.editor import *
+import os
 
 def render_video(audio_path, subtitles, output):
-    video = VideoFileClip("background.mp4").subclip(0, 30)
+    if not os.path.exists("background.mp4"):
+        print("⚠️ Không có background → dùng video màu đen")
+
+        video = ColorClip(size=(720, 1280), color=(0,0,0), duration=30)
+    else:
+        video = VideoFileClip("background.mp4").subclip(0, 30)
 
     audio = AudioFileClip(audio_path)
     video = video.set_audio(audio)
