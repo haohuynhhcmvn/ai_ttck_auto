3699649475d7ebed49330c301c4fb047ec01079e
haohuynhhcmvn <haohuynh.hcm.vn@gmail.com>
2026-03-22 20:23:16 +0700

    Update subtitle.py

--- subtitle.py
+++ subtitle.py
@@ -7,15 +7,22 @@ from moviepy.editor import TextClip
 
 def create_subtitles(words):
     clips = []
-    for w in words:
-        clip = TextClip(
-            w["word"],
-            fontsize=70,
-            color="yellow",
-            stroke_color="black",
-            stroke_width=2
-        ).set_start(w["start"]).set_duration(w["end"] - w["start"]).set_position(("center","bottom"))
 
-        clips.append(clip)
+    for w in words:
+        try:
+            clip = TextClip(
+                w["word"],
+                fontsize=60,
+                color="white",
+                font="DejaVu-Sans",  # 🔥 FIX FONT
+                method="caption"     # 🔥 FIX ImageMagick
+            ).set_start(w["start"]).set_duration(
+                w["end"] - w["start"]
+            ).set_position(("center", "bottom"))
+
+            clips.append(clip)
+
+        except Exception as e:
+            print("⚠️ lỗi subtitle:", e)
 
     return clips
