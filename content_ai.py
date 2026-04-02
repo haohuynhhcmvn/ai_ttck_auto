6f89d351d466bc110564791d786d2b343a868f05
haohuynhhcmvn <haohuynh.hcm.vn@gmail.com>
2026-04-02 20:55:42 +0700

    Update content_ai.py

--- content_ai.py
+++ content_ai.py
@@ -15,7 +15,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 NVIDIA_API_KEY = os.getenv("NVIDIA_API_KEY")
 
 QWEN_MODEL = "qwen/qwen3.5-122b-a10b"
-GEMINI_MODEL = "gemini-1.5-flash" # Dùng bản Flash để tối ưu tốc độ cho Social
+GEMINI_MODEL = "gemini-2.5-flash" # Dùng bản Flash để tối ưu tốc độ cho Social
 
 # ==============================
 # LLM CALLS
